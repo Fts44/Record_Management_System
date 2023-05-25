2c46868656dc949b51f4068ea93542bca5b7e433
Fts44 <calmajoseph7@gmail.com>
2023-05-25 19:44:57 +0800

    added patient sidebar

---
+++ resources/views/Components/Patient/Sidebar.blade.php
@@ -0,0 +1,109 @@
+<!-- Sidebar -->
+<nav class="navbar-vertical navbar">
+    <div class="nav-scroller">
+        <!-- Brand logo -->
+        <a class="navbar-brand d-flex">
+            <!-- <img src="{{ asset('assets/photos/logo_w_text.png') }}" alt=""/> -->
+            <span>Health Portal</span>
+        </a>
+
+        <!-- Navbar nav -->
+        <ul class="navbar-nav flex-column" id="sideNavbar">
+
+            <li class="nav-item">
+                <a class="nav-link has-arrow " href="#">
+                    <i data-feather="pie-chart" class="nav-icon icon-xs me-2"></i>  Dashboard
+                </a>
+            </li>
+
+            <!-- Nav Heading -->
+            <li class="nav-item">
+                <div class="navbar-heading">Settings</div>
+            </li>  
+
+            <li class="nav-item">
+                <a class="nav-link active" href="">
+                    <i class="nav-icon icon-xs me-2 bi bi-person"></i> Profile
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="">
+                    <i class="nav-icon icon-xs me-2 bi bi-telephone"></i> Emergency Contact
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="">
+                    <i class="nav-icon icon-xs me-2 bi bi-gear"></i> Change Password
+                </a>
+            </li>
+
+            <!-- <li class="nav-item">
+                <a class="nav-link has-arrow  collapsed " href="#" data-bs-toggle="collapse" data-bs-target="#navMenuLevel" aria-expanded="false" aria-controls="navMenuLevel">
+                    <i data-feather="corner-left-down"class="nav-icon icon-xs me-2"></i> Menu
+                </a>
+
+                <div id="navMenuLevel" class="collapse " data-bs-parent="#sideNavbar">
+                    <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nav 1</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link has-arrow " href="#" data-bs-toggle="collapse" data-bs-target="#navMenuLevelSecond" aria-expanded="false" aria-controls="navMenuLevelSecond">
+                            Nav 2
+                        </a>
+
+                        <div id="navMenuLevelSecond" class="collapse" data-bs-parent="#navMenuLevel">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link " href="#">Item 1</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="#">Item 2</a>
+                            </li>
+                        </ul>
+                        </div>
+                    </li>
+                    </ul>
+                </div>
+            </li> -->
+
+            <!-- Nav Heading -->
+            <li class="nav-item">
+                <div class="navbar-heading">Records</div>
+            </li>  
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-xs me-2 bi bi-file-earmark-text"></i> Dental Certificate
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-xs me-2 bi bi-file-earmark-text"></i> Excuse Slip
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-xs me-2 bi bi-file-earmark-text"></i> Medical Certificate
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-xs me-2 bi bi-file-earmark-text"></i> Medical Referral Form
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-xs me-2 bi bi-file-earmark-text"></i> Medical Request Slip
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
