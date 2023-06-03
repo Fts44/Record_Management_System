9e108eaad62991c19c89ff11b653e1d7515c10f8
Fts44 <calmajoseph7@gmail.com>
2023-06-04 04:40:13 +0800

    change the type of religion field

--- database/migrations/2023_04_01_061448_create_personal_information_table.php
+++ database/migrations/2023_04_01_061448_create_personal_information_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id('pi_id');
             $table->integer('acc_id')->unique();
             $table->integer('add_id')->unique()->nullable();
+            $table->integer('rlgn_id')->nullable();
             $table->string('pi_classification');
             $table->string('pi_position')->nullable();
             $table->string('pi_firstname')->nullable();
@@ -25,7 +26,6 @@ return new class extends Migration
             $table->string('pi_sex')->nullable();
             $table->date('pi_birthdate')->nullable();
             $table->string('pi_civil_status')->nullable();
-            $table->string('pi_religion')->nullable();
             $table->string('pi_grade_level')->nullable();
             $table->integer('dept_id')->nullable();
             $table->integer('crs_id')->nullable();
