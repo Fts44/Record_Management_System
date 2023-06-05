e2ae1a7d88595856cc3347cfe0a683a430529411
Fts44 <calmajoseph7@gmail.com>
2023-06-05 23:36:20 +0800

    fix notification display

--- resources/views/Components/Patient/Header.blade.php
+++ resources/views/Components/Patient/Header.blade.php
@@ -59,7 +59,7 @@
                         <ul class="list-group list-group-flush notification-list-scroll bg-light">
                             <!-- List group item -->
 
-                            @if($notification['incomplete_data']['personal_information'])
+                            @if(isset($notification['incomplete_data']['personal_information']))
                                 <li class="list-group-item border-bottom">
                                     <a href="{{ route('Patient.Profile.Index') }}" class="text-muted">
                                         <h5 class="mb-1 fw-">Missing Data in Personal Information</h5>
@@ -70,7 +70,7 @@
                                 </li>
                             @endif
 
-                            @if($notification['incomplete_data']['emergency_contact'])
+                            @if(isset($notification['incomplete_data']['emergency_contact']))
                                 <li class="list-group-item border-bottom">
                                     <a href="{{ route('Patient.Profile.Index') }}" class="text-muted">
                                         <h5 class="mb-1 fw-">Missing Data in Emergency Contact</h5>
@@ -97,15 +97,15 @@
             <li class="dropdown ms-2">
                 <a class="rounded-circle" href="#" role="button" id="dropdownUser" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <div class="avatar avatar-md avatar-indicators avatar-online">
-                        <img alt="avatar" src="{{ asset('assets/photos/default-profile.jpg') }}" class="rounded-circle" />
+                        <img alt="avatar" id="header_avatar" src="{{ ($profile_picture) ? asset('storage/photos/'.$profile_picture) : asset('assets/photos/default-profile.jpg')  }}" class="rounded-circle" />
                     </div>
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownUser">
                     <div class="px-4 pb-0 pt-2">
                         <div class="lh-1">
-                            <h5 class="mb-1 display-name">{{ $first_name.' '.$middle_name[0].'. '.$last_name }}</h5>
-                            <span class="text-inherit fs-6">{{ ($acc_type=='Standard') ? $position : $acc_type }}</span>
+                            <h5 class="mb-1 display-name" id="header_name">{{ $first_name.' '.$last_name }}</h5>
+                            <span class="text-inherit fs-6" id="header_position">{{ ($acc_type=='Standard') ? $position : $acc_type }}</span>
                         </div>
                         <div class=" dropdown-divider mt-3 mb-2"></div>
                     </div>
