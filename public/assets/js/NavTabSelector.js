9d63ca148807584f4c82705a7a42a73e66c08427
Fts44 <calmajoseph7@gmail.com>
2023-06-20 16:39:29 +0800

    added nav-tab selector function

---
+++ public/assets/js/NavTabSelector.js
@@ -0,0 +1,19 @@
+$(function() {
+
+    // check if there is a nav-link on the html page
+    if ($("#content a.nav-link").length > 0){ 
+
+        // set last active tab on localStorage
+        $('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+            localStorage.setItem('lastTab', $(this).attr('href'));
+        });
+
+        // get and set the last tab active
+        var lastTab = localStorage.getItem('lastTab');
+        lastTab = lastTab ?  $('[href="' + lastTab + '"]') : $('a[data-bs-toggle="tab"]').first();
+        lastTab.tab('show');
+    }
+
+    // show the content div
+    $("#content").removeClass('d-none');
+}); 
