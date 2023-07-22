05fef145ba61cd4d59100cb4de98b50eafee4f4f
Fts44 <calmajoseph7@gmail.com>
2023-07-22 18:12:57 +0800

    added electronic_record table

--- database/migrations/2023_06_20_231652_create_electronic_record_table.php
+++ database/migrations/2023_06_20_231652_create_electronic_record_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->datetime('er_created_date');
             $table->datetime('er_doc_last_update')->nullable();
             $table->integer('er_doc_org_id');  // original id base on their respective table
+            $table->string('er_control_no')->nullable(); // for documents needed customize control no
             $table->integer('dt_id');
             $table->integer('physician_id');
             $table->integer('patient_id');
